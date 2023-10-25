19dc55ff69d5d053752679601856f6dfa453f274
abubakir88 <abubakirsaidxonov88@gmail.com>
2023-10-25 19:17:08 +0500

    some full

---
+++ js/courses.js
@@ -0,0 +1,40 @@
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// BACK-TOP
+
+const header = document.querySelector("header");
+const menuBtn = document.querySelector(".menu");
+
+menuBtn.addEventListener("click", () => {
+  header.classList.toggle("open");
+});
+
+// NAV TOGGLE
+
+let modeBtn = document.getElementById("dark-light");
+
+modeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+});
+
+// DARK MODE
